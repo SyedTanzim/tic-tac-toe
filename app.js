a23b9d863c9278fcab05bd7274653229dd62034c
tanzim <syedtanzimwajih@gmail.com>
2026-01-15 22:11:57 +0530

    feat: add board rendering function

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ let board = [];
         board.push(internalBoard);
     }
 })();
+
+function RenderBoard() {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (board[i][j] != null) {
+                process.stdout.write(` ${board[i][j]} `);
+            } else {
+                process.stdout.write(' . ');
+            }
+        }
+        console.log();
+    }
+};
