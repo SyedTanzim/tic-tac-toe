39968ceaf5b795e71dc9405ad1f6ba7fa49e06a9
tanzim <syedtanzimwajih@gmail.com>
2026-01-18 18:25:21 +0530

    feat: implement 3x3 board rendering with HTML, CSS, and JS

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const prompt = require("prompt-sync")({ sigint: true });
-
 let row = 3;
 let col = 3;
 let board = [];
@@ -15,130 +13,129 @@ let board = [];
 })();
 
 function RenderBoard() {
+
+    let container = document.querySelector('#blocksContainer');
+
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
-            if (board[i][j] != null) {
-                process.stdout.write(` ${board[i][j]} `);
-            } else {
-                process.stdout.write(' . ');
-            }
+            let block = document.createElement('div');
+            block.className = 'block';
+            block.addEventListener('click', () => {
+                console.log(i, j);
+            })
+            container.appendChild(block);
         }
-        console.log();
     }
 };
 
 RenderBoard();
 
-function PlayerFactory(name, symbol) {
-    return { name, symbol };
-};
+// function PlayerFactory(userName , symbol) {
+//     return {userName , symbol};
+// };
 
-const player1 = PlayerFactory('Tanzim', 'X');
-const player2 = PlayerFactory('Himesh', 'O');
+// const player1 = PlayerFactory(prompt('Enter player1 Name: '), 'X');
+// const player2 = PlayerFactory(prompt('Enter player1 Name: '), '0');
 
-function InputManager(i, j, value) {
-    board[i][j] = value;
-};
+// function WinChecker() {
 
-function WinChecker() {
+//     if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != null) {
+//         return true;
+//     }
 
-    if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != null) {
-        return true;
-    }
+//     if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != null) {
+//         return true;
+//     }
 
-    if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != null) {
-        return true;
-    }
+//     if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != null) {
+//         return true;
+//     }
 
-    if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != null) {
-        return true;
-    }
+//     if (board[1][1] == board[0][0] && board[1][1] == board[2][2] && board[1][1] != null) {
+//         return true;
+//     }
 
-    if (board[1][1] == board[0][0] && board[1][1] == board[2][2] && board[1][1] != null) {
-        return true;
-    }
+//     if (board[1][1] == board[0][2] && board[1][1] == board[2][0] && board[1][1] != null) {
+//         return true;
+//     }
 
-    if (board[1][1] == board[0][2] && board[1][1] == board[2][0] && board[1][1] != null) {
-        return true;
-    }
+//     if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != null) {
+//         return true;
+//     }
 
-    if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != null) {
-        return true;
-    }
+//     if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != null) {
+//         return true;
+//     }
 
-    if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != null) {
-        return true;
-    }
+//     if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != null) {
+//         return true;
+//     }
 
-    if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != null) {
-        return true;
-    }
+//     return false;
 
-    return false;
+// }
 
-}
+// function TurnManager() {
+//     let move = ''
+//     let name = '';
 
-function TurnManager() {
-    let move = ''
-    let name = '';
+//     function setMoveAndName() {
+//         if (move == '' || move == player2.symbol) {
+//             move = player1.symbol;
+//             name = player1.name;
+//         } else {
+//             move = player2.symbol;
+//             name = player2.name;
+//         }
+//     }
 
-    function setMoveAndName() {
-        if (move == '' || move == player2.symbol) {
-            move = player1.symbol;
-            name = player1.name;
-        } else {
-            move = player2.symbol;
-            name = player2.name;
-        }
-    }
+//     function getMove() {
+//         return move;
+//     }
+//     function getName(){
+//         return name;
+//     }
+//     return { getMove, getName, setMoveAndName };
+// };
 
-    function getMove() {
-        return move;
-    }
-    function getName(){
-        return name;
-    }
-    return { getMove, getName, setMoveAndName };
-};
+// const turn = TurnManager();
 
-const turn = TurnManager();
+// function GameFlow() {
+//     let count = row * col;
 
-function GameFlow() {
-    let count = row * col;
+//     while (count > 0) {
 
-    while (count > 0) {
+//         let rowInput = parseInt(prompt('enter row b/w 0-2: '));
+//         if (rowInput > 2 || rowInput < 0) {
+//             console.log('Invalid Input, Try again');
+//             continue;
+//         }
 
-        let rowInput = parseInt(prompt('enter row b/w 0-2: '));
-        if (rowInput > 2 || rowInput < 0) {
-            console.log('Invalid Input, Try again');
-            continue;
-        }
+//         let colInput = parseInt(prompt('enter col b/w 0-2: '));
+//         if (colInput > 2 || colInput < 0) {
+//             console.log('Invalid Input, Try again');
+//             continue;
+//         }
 
-        let colInput = parseInt(prompt('enter col b/w 0-2: '));
-        if (colInput > 2 || colInput < 0) {
-            console.log('Invalid Input, Try again');
-            continue;
-        }
+//         if (board[rowInput][colInput] != null) {
+//             console.log('Cell already occupied');
+//             continue;
+//         }
 
-        if (board[rowInput][colInput] != null) {
-            console.log('Cell already occupied');
-            continue;
-        }
+//         turn.setMoveAndName();
+//         InputManager(rowInput, colInput, turn.getMove());
+//         count--;
+//         RenderBoard();
 
-        turn.setMoveAndName();
-        InputManager(rowInput, colInput, turn.getMove());
-        count--;
-        RenderBoard();
+//         let result = WinChecker();
 
-        let result = WinChecker();
+//         if (result == true) {
+//             console.log(turn.getName(), 'Won the game');
+//             break;
+//         } else if (count == 0) {
+//             console.log('Game is Draw');
+//         }
+//     }
+// };
 
-        if (result == true) {
-            console.log(turn.getName(), 'Won the game');
-            break;
-        } else if (count == 0) {
-            console.log('Game is Draw');
-        }
-    }
-};
-
-GameFlow();
+// GameFlow();
